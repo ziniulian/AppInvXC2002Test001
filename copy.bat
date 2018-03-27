da5dc77303f500c5438dc8b1acf526e8b9029e35
ziniulian <ziniulian@163.com>
2018-03-27 16:48:03 +0800

    初建

---
+++ copy.bat
@@ -0,0 +1,5 @@
+@echo off
+xcopy E:\LZR\Doc\ASwork\XC2002Test001\app\src\main E:\LZR\Doc\Git\AppInvXC2002Test001\app\src\main\ /S
+xcopy E:\LZR\Doc\ASwork\XC2002Test001\app\libs E:\LZR\Doc\Git\AppInvXC2002Test001\app\libs\ /S
+copy E:\LZR\Doc\ASwork\XC2002Test001\app\build.gradle E:\LZR\Doc\Git\AppInvXC2002Test001\app
+pause
